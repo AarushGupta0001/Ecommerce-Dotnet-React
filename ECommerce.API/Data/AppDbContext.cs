cc8bd5bb46f4ca376acddd638c945f4b800ae771
AarushGupta0001 <dicsag@gmail.com>
2026-02-13 11:59:28 +0530

    feat: add order and order item models

--- ECommerce.API/Data/AppDbContext.cs
+++ ECommerce.API/Data/AppDbContext.cs
@@ -11,6 +11,9 @@ namespace ECommerce.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
 
     }
 }
---
+++ ECommerce.API/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerce.API.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public decimal TotalAmount { get; set; }
+
+        public List<OrderItem> Items { get; set; } = new();
+    }
+}
