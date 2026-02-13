319547f54c910f1981af024762fe875ca36b3749
AarushGupta0001 <dicsag@gmail.com>
2026-02-13 11:07:46 +0530

    feat: add User model and Users table

--- ECommerce.API/Data/AppDbContext.cs
+++ ECommerce.API/Data/AppDbContext.cs
@@ -8,5 +8,7 @@ namespace ECommerce.API.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+
     }
 }
